325c62c253d958434beda08adb5fd2b4b2846a04
Jackson Dushane <jkd5869@rit.edu>
2019-02-16 13:26:46 -0500

    Added ShoppingCart class skeleton, take 2

--- app/src/main/java/com/main/weggies/ShoppingCart.java
+++ app/src/main/java/com/main/weggies/ShoppingCart.java
@@ -1,7 +1,13 @@
 package com.main.weggies;
 
-/**Used to display products in your personal cart
+/** Used to display products in your personal cart
+ * Shopping cart is hashmap of <product, amount> of products
+ * Each product has all of the information in it to look 'nice' in cart
  *
  */
 public class ShoppingCart {
+    private int size = 0;
+    private double total = 0.0;
+
 }
+
