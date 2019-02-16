f14edbda0ea5e9c214e7bbceab4e7b43e9f37f10
Hendrick Ducasse <hd1867@rit.edu>
2019-02-16 17:13:33 -0500

    Added Store Location Function

--- app/src/main/java/com/main/weggies/MainActivity.java
+++ app/src/main/java/com/main/weggies/MainActivity.java
@@ -1,6 +1,13 @@
 package com.main.weggies;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -9,10 +16,33 @@ public class MainActivity extends AppCompatActivity {
 
     float budget;
     int household;
-    Store store;
 
     String BUDGET = "com.main.weggies.BUDGET";
     String HOUSEHOLD = "com.main.weggies.HOUSEHOLD";
+
+    LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +50,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void proceed(){
+    public void proceed() {
         Intent intent = new Intent(this, ScrollingActivity.class);
-        EditText budgetTxt = (EditText)findViewById(R.id.budget);
-        EditText householdTxt = (EditText)findViewById(R.id.household);
+        EditText budgetTxt = (EditText) findViewById(R.id.budget);
+        EditText householdTxt = (EditText) findViewById(R.id.household);
 
         int budget = Integer.parseInt(budgetTxt.getText().toString());
         int household = Integer.parseInt(householdTxt.getText().toString());
@@ -35,7 +65,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void getStore(){
+    public storeLocation getUserLocation() {
+        String locationProvider = LocationManager.GPS_PROVIDER;
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return new storeLocation(0, 0);
+        }
+        Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+
+        double latitude = lastKnownLocation.getLatitude();
+        double longitude = lastKnownLocation.getLongitude();
+
+        return new storeLocation(latitude, longitude);
+
+
+
 
     }
 
---
+++ app/src/main/java/com/main/weggies/storeLocation.java
@@ -0,0 +1,11 @@
+package com.main.weggies;
+
+public class storeLocation {
+    double lat;
+    double lon;
+
+    public storeLocation(double lat, double lon){
+        this.lat = lat;
+        this.lon = lon;
+    }
+}
