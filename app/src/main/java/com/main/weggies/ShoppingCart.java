86be9bd157465019d178b46e603da31857f03654
Jackson Dushane <jkd5869@rit.edu>
2019-02-16 15:13:00 -0500

    Quick commit

--- app/src/main/java/com/main/weggies/ShoppingCart.java
+++ app/src/main/java/com/main/weggies/ShoppingCart.java
@@ -1,13 +1,65 @@
 package com.main.weggies;
 
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import java.util.HashMap;
+
 /** Used to display products in your personal cart
  * Shopping cart is hashmap of <product, amount> of products
  * Each product has all of the information in it to look 'nice' in cart
  *
+ * OPERATORS:
+ * - addXProduct: Adds x amount of a product, increases size and total
+ * - removeXProduct: Removes x amount of a product, increases size and total
  */
 public class ShoppingCart {
-    private int size = 0;
-    private double total = 0.0;
+    private int size;
+    private double total; //All item prices, + price*tax
+    private HashMap cart; //TODO specify as product, amount
+
+    /**
+     * ShoppingCart constructor
+     * Generates initial size(0), cart total(0.0) and a default Hmap representing the cart
+     */
+    public ShoppingCart(){
+        size = 0;
+        total = 0.0;
+        cart = new HashMap();
+    }
+
+    /**
+     * Adds product(s) to a cart
+     * updates size and total
+     * @param item a product
+     * @param amount integer marking amount of products added
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void addXProduct(Product item, int amount){
+        size += amount;
+        if (cart.containsKey(item)){
+            cart.replace(item, cart.get(item) + amount); //Keep an eye on this warning
+        }
+        else cart.put(item, amount);
+        total = item.getPrice; //TODO actually find a way to calculate total after add
+    }
+
+    /**
+     * Removes product(s) from a cart
+     * updates size and total
+     * @param item a product
+     * @param amount integer marking amount of products added
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void removeXProduct(Product item, int amount){
+        size -= amount;
+        if (cart.get(item) <= amount){
+            cart.remove(item);
+        }
+        else cart.replace(item, cart.get(item) - amount);
+
+        total =  69.69; //TODO actually find a way to calculator total after remove
+    }
 
 }
 
