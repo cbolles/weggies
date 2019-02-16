e307ff0fdd69ac97a5ac4a0a9ac768c1de7db8fb
Hendrick Ducasse <hd1867@rit.edu>
2019-02-16 14:30:07 -0500

    Main Activity Finished

--- app/src/main/java/com/main/weggies/ScrollingActivity.java
+++ app/src/main/java/com/main/weggies/ScrollingActivity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class CartDisplay extends AppCompatActivity {
+public class ScrollingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart_display);
+        setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
