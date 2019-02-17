e87d74787b4127683a84f5fcfdf5405f3d2c05b4
Hendrick Ducasse <hd1867@rit.edu>
2019-02-16 23:35:15 -0500

    NIOFBWDOCWAFNNIOFEANOISAFNOIFEANOEAFNIO

--- app/src/main/java/com/main/weggies/RecipeSelectActivity.java
+++ app/src/main/java/com/main/weggies/RecipeSelectActivity.java
@@ -2,6 +2,7 @@ package com.main.weggies.searchview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListAdapter;
 
 import com.main.weggies.R;
 
