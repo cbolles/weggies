fd86b94d6021a023d111efecdb59e6e9e05ba413
Jackson Dushane <jkd5869@rit.edu>
2019-02-16 20:29:54 -0500

    starting recipekeygen and userkeygen

--- app/src/main/java/com/main/weggies/RecipeKeyGen.java
+++ app/src/main/java/com/main/weggies/RecipeKeyGen.java
@@ -6,6 +6,9 @@ public class RecipeKeyGen {
     private Array reciReg;
 
     public RecipeKeyGen(){
-
+        //For each ingredient, look at the sku and find the product from that sku
+        // generate key from each product (or take pregenerated one) and apply bitwise operation
+        // To all of these prodcuts to get a recipe key
+        boolean[] reciReg = {true, true, true, true, true, true, true, true, true};
     }
 }
---
+++ app/src/main/java/com/main/weggies/UserKeyGen.java
@@ -0,0 +1,8 @@
+package com.main.weggies;
+
+/**
+ * Uses user input to create a keygen similar to ProdcutKeyGen
+ *
+ */
+public class UserKeyGen {
+}
